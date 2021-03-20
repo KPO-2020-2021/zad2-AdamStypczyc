11b234128c165f51a58c623b83fd1f3b9f1676c6
Adam  Stypczyc <259273@student.pwr.edu.pl>
2021-03-20 14:14:16 +0100

    tydzień 0 i 1, testy i przeciążenia

--- inc/Statystyki.hh
+++ inc/Statystyki.hh
@@ -1,13 +1,21 @@
-#ifndef WYRAZENIEZESP_HH
-#define WYRAZENIEZESP_HH
+#ifndef STATYSTYKI_HH
+#define STATYSTYKI_HH
 
 
-
+#include <iostream>
 #include "LZespolona.hh"
 #include "WyrazenieZesp.hh"
 
 /*
  * Tu nalezy zdefiniowac funkcje, ktore definiuja model statystyki 
  */
+using namespace std;
 
-#endif
+struct Statystyka
+{
+    int Odpowiedzi;
+    int Dobre;
+    int Zle;
+};
+
+#endif //STATYSTYKI_HH
